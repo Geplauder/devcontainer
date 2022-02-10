e9bfdc8fe9ff283c16c536c82d2981a6a8131cb4
Fabian Vowie <fabian@vowie.me>
2022-02-10 20:23:21 +0100

    Only run init.sh on app container creation

--- init.sh
+++ init.sh
@@ -26,11 +26,30 @@ if [ ! -d "./oxygen/.git" ]; then
 	echo "Oxygen not found, cloning..."
 	rm -r oxygen
 	git clone https://github.com/Geplauder/oxygen.git oxygen
-	echo "Oxygen cloned successfully"
+
+	echo "Oxygen cloned successfully, installing npm packages..."
+
+	cd ./oxygen
+	npm i
+
+	echo "Oxygen npm packages successfully installed."
+	cd ./..
+
+	echo "Successfully initialized Oxygen."
 else
 	echo "Found existing Oxygen directory, skipping..."
 fi
 
-echo "Repositories successfully initialized, sleeping..."
+echo "Repositories successfully initialized"
 
-sleep infinity
+# cd ./ferrum
+# nohup bash -c "cargo watch -x 'run --bin ferrum' &"
+# cd ./ferrum-websocket
+# nohup bash -c "cargo watch -x 'run --bin ferrum-websocket' &"
+
+# cd ../../oxygen
+# nohup bash -c "yarn dev"
+
+# cd ./../
+
+# sleep infinity
